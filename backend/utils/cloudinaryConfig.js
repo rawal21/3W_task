174628680968d5bcf51ecc7a43e29e4d41681e56
dikshitkumar <dikshitrawal923@gmail.com>
2025-01-20 13:25:37 +0530

    edit the name of cloud_secret

--- backend/utils/cloudinaryConfig.js
+++ backend/utils/cloudinaryConfig.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME, // Replace with your Cloudinary cloud name
   api_key: process.env.CLOUD_KEY,       // Replace with your Cloudinary API key
-  api_secret: process.env.CLOUD_SECERT,  // Replace with your Cloudinary API secret
+  api_secret: process.env.CLOUD_SECRET,  // Replace with your Cloudinary API secret
 });
 
 module.exports = cloudinary;
